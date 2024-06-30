39c641dd7398cef3091e301676fb1029a5f0b3ee
NF800 <zhaonpc@outlook.com>
2024-06-30 14:31:37 +0800

    新增员工业务代码

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -1,15 +1,20 @@
 package com.sky.config;
 
 import com.sky.interceptor.JwtTokenAdminInterceptor;
+import com.sky.json.JacksonObjectMapper;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 //import springfox.documentation.builders.ApiInfoBuilder;
 //import springfox.documentation.builders.PathSelectors;
 //import springfox.documentation.builders.RequestHandlerSelectors;
@@ -80,4 +85,31 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
                         .version("1.0")
                         .description("This is my API documentation."));
     }
+
+//    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        log.info("扩展消息转换器...");
+//        //创建消息转换器对象
+//        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+//        //设置对象转换器，底层使用Jackson将Java对象转为json
+//        messageConverter.setObjectMapper(new JacksonObjectMapper());
+//        //将上面的消息转换器对象追加到mvc框架的转换器集合中
+//        converters.add(0, messageConverter);
+//    }
+
+    /**
+     * 扩展消息转换器,将日期类型从列表转换为时间戳
+     * 这个是导致knife4j不能正常显示的罪魁祸首,特别要注意添加的位置
+     * @param converters 消息转换器列表
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //创建消息转换器对象
+        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        //设置对象转换器，底层使用Jackson将Java对象转为json
+        jackson2HttpMessageConverter.setObjectMapper(new JacksonObjectMapper());
+        //将上面的消息转换器对象追加到mvc框架的转换器集合中
+        //添加到集合中的位置,0表示第一个,1表示第二个,以此类推
+        converters.add(converters.size()-1,jackson2HttpMessageConverter);
+    }
+
 }
